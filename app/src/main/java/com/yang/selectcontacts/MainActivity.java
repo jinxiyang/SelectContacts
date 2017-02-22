8343319aefcb220595b1013678751f746c279b0f
yangjinxi <18817570698@qq.com>
2017-02-22 21:40:46 +0800

    create project

---
+++ app/src/main/java/com/yang/selectcontacts/MainActivity.java
@@ -0,0 +1,30 @@
+package com.yang.selectcontacts;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_selected_contacts)
+    TextView tvSelectedContacts;
+    @BindView(R.id.btn_select)
+    Button btnSelect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btn_select)
+    public void onClick() {
+
+    }
+}
